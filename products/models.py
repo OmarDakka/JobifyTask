e8279a2cf8ac438d770d728469f6f872a34a8251
OmarDakka <omardakka42@gmail.com>
2021-10-28 18:51:10 +0300

    register application to admin

--- products/models.py
+++ products/models.py
@@ -3,15 +3,14 @@ from django.db import models
 # Create your models here.
 
 
+class Category(models.Model):
+    title = models.CharField(blank=False, max_length=255)
+
 class Product(models.Model):
     title = models.CharField(blank=False, max_length=255)
     description = models.CharField(blank=False, max_length=255)
-
+    category = models.ManyToManyField(Category, related_name="product_category")
     price = models.FloatField(blank=False)
     image = models.CharField(blank=False, max_length=255)
     available_quantity = models.IntegerField(blank=False)
 
-
-class Category(models.Model):
-    title = models.CharField(blank=False, max_length=255)
-    product = models.ManyToManyField(Product, related_name="product_category")
